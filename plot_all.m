73c610ab153f0df26e86d620b468367b4e47b5a9
k-schmid <konstantin-schmid@web.de>
2016-11-18 16:25:26 +0100

    Changed viewing angle to 180

--- plot_all.m
+++ plot_all.m
@@ -4,17 +4,17 @@ clc
 layer = 7:8;
 rangeLimit = 100;
 precision = 0;
-viewing_angle = [deg2rad(0),deg2rad(360)];% [deg2rad(0),deg2rad(90);deg2rad(270),deg2rad(360)];
+viewing_angle = [deg2rad(0),deg2rad(90);deg2rad(270),deg2rad(360)];
 outlier_range = deg2rad(5);
 center_statistics = 'median';
 num_bins = 180;
-reload = false;
+reload = true;
 verbose = false;
 plot_path = '../Evaluation/';
 addpath('..');
 addpath('../../tools/')
 data_path = get_dataPath();
-parfor intersections=1:22
+for intersections=2%1:22
     clouds_path = [data_path int2str(intersections) '/'];
     fprintf('Intersection %d\n',intersections)
     %% Defaults
@@ -22,6 +22,7 @@ parfor intersections=1:22
     plot_intersection(clouds_path,layer,outlier_range, rangeLimit, viewing_angle,precision,center_statistics,num_bins,reload,true,verbose);
     filename =sprintf('Isovists %d_%d bins_%s.png',intersections,num_bins,center_statistics);
     set(gcf, 'Position', get(0, 'Screensize'));
+    return
     mkdir(path)
     export_fig([path,filename],'-pdf','-m3','-transparent')
     
